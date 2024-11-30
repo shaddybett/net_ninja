fef01d6f17a4f6915793757aebb057a10be6b580
Shaddy <shadybett540@gmail.com>
2024-11-30 14:15:51 +0300

    made app bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-
+    home: Home(),
   ));
 }
 
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ninja Id')
+      ),
+    );
+  }
+}
