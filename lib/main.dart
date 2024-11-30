86febdd04f0cf7c28856270ded0a644d5ef540e8
Shaddy <shadybett540@gmail.com>
2024-11-30 14:55:12 +0300

    added a height for spaces

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,9 @@ class Home extends StatelessWidget {
         ),),
         centerTitle: true,
       ),
-      body: Column(
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text('NAME',
@@ -31,6 +33,7 @@ class Home extends StatelessWidget {
             color: Colors.grey,
             letterSpacing: 2.0,
           ),),
+          SizedBox(height: 10.0),
           Text('Chun-Li',
           style: TextStyle(
             color: Colors.amberAccent[400],
@@ -38,8 +41,23 @@ class Home extends StatelessWidget {
             fontSize: 20.0,
             fontWeight: FontWeight.bold,
             fontFamily: 'PlayfairDisplay'
-          ),)
-        ],
+          ),),
+          SizedBox(height: 30.0),
+          Text('Ninja Level',style: TextStyle(
+            color: Colors.grey,
+            letterSpacing: 2.0
+          ),),
+          SizedBox(height: 10.0,),
+          Text('8',style: TextStyle(
+            color: Colors.amberAccent[400],
+            letterSpacing: 2.0,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'PlayfairDisplay'
+          ),),
+          SizedBox(height: 30,)
+        ]
+        ),
       ),
     );
   }
