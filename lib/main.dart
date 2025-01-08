3521a56eb2d0059ac5b5416a3495cc1904cf2e04
Shaddy <shadybett540@gmail.com>
2025-01-08 21:16:24 +0300

    uncommented field for fonts

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,12 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   home: Scaffold(
     appBar: AppBar(
-      title: Text('My app'),
+      title: Text('My app',style: TextStyle(color: Colors.white),),
       centerTitle: true,
+      backgroundColor: Colors.blue[400],
     ),
     body: Center(
-      child: Text('Hello ninjas'),
+      child: Text('Hello ninjas',style: TextStyle(fontWeight: FontWeight.bold,letterSpacing: 2.0,fontSize: 10.0),),
     ),
     floatingActionButton: FloatingActionButton(
       onPressed: ()=>{},
