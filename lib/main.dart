ce167f6e9a277d9c0ccd7d6932da59b8b2072939
Shaddy <shadybett540@gmail.com>
2025-01-08 20:59:30 +0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,17 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('My app'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text('Hello ninjas'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: ()=>{},
+      child: Text('Click'),
+    ),
+  ),
 ));
